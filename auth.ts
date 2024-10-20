bcef85c101a6ec8dcdf3c6815a035cfef06d357b
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2024-10-20 16:16:51 +0200

    first commit

---
+++ auth.ts
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import { ZodError } from "zod";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./lib/zod";
+import { prisma } from "@/lib/prisma";
+import { verifyPassword } from "@/utils/password";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          const { email, password } = await signInSchema.parseAsync(
+            credentials
+          );
+
+          // Look up the user in the database
+          const user = await prisma.user.findUnique({
+            where: { email },
+          });
+
+          // If no user is found or the password is incorrect, return null
+          if (!user) {
+            console.error("User not found");
+            return null;
+          }
+
+          const isPasswordValid = await verifyPassword(password, user.password);
+          if (!isPasswordValid) {
+            console.error("Invalid password");
+            return null;
+          }
+
+          // Return the user object if valid
+          return { id: user.id, name: user.name, email: user.email };
+        } catch (error) {
+          if (error instanceof ZodError) {
+            console.error("Validation error:", error.errors);
+            return null;
+          }
+
+          console.error("Authorization error:", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
